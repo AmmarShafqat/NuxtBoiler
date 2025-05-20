58835fd5e697f3af79dec738592cecc49e934d96
Ammar Shafqat <ammarshafqat727@gmail.com>
2025-05-20 22:36:32 +0500

    nuxt3

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,3 +1,19 @@
 <template>
-<h1>Header Component</h1>
+    <div class="bg-[aqua]">
+       <div class="container mx-auto">
+         <div class="flex justify-between">
+            <div>
+                <h2>Logo</h2>
+            </div>
+            <ul class="flex gap-2">
+                <li v-for="item in menuItems">{{item?.title}}</li>
+            </ul>
+
+         </div>
+       </div>
+    </div>
 </template>
+<script setup lang="ts">
+const config = useRuntimeConfig();
+const menuItems = config?.public?.MENU_ITEMS
+</script>
--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,10 @@
 export default {
-    SITE_URL: 'Demo Site'
+    SITE_URL: 'Demo Site',
+    MENU_ITEMS: [
+      {id:1,title:'Home',url:'/'},
+      {id:2,title:'About',url:'/'},
+      {id:3,title:'Services',url:'/'},
+      {id:4,title:'Contact Us',url:'/'},
+    ]
   };
   
