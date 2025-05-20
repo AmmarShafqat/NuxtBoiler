cf1df0e8a6f4c7fd1db4edf9531bb7b48d0bf7ac
Ammar Shafqat <ammarshafqat727@gmail.com>
2025-05-20 22:05:09 +0500

    nuxt 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 import config from "./config"
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    }
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/image'],
   css: ['~/assets/css/main.css'],
